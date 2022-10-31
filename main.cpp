9eaf56bacf1fd1da83ee0170eef2c8a6847b8be8
Rufelle Emmanuel Pactol <rufelleemmanuelpactol7@gmail.com>
2022-10-31 23:59:00 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	cmake-build-debug/Testing/Temporary/LastTest.log

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main (){
     Memory * memory = new Memory();
     Rscpu * rscpu = new Rscpu(memory);
     Cmd * terminal = new Cmd(memory);
+    std::cout << "Now running the RSCPU interpreter...\n";
     terminal->scan();
     std::cout << "Press enter key to quit process... ";
 	getchar();
